4ce9688c63af50ae964d3fe17ae84001ec886ed1
prasanthdeveloper <39476222+prasanthandroiddeveloper@users.noreply.github.com>
2018-06-02 13:12:00 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/badari/mks/MainActivity.java
+++ app/src/main/java/com/example/badari/mks/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-               /* public void openingss( View v){
+               /* public void openingssss( View v){
                 if(usr.getText().toString().ccccequals("Admin") &&(psd.getText().toString().equals("Admin123") &&usr.length()>1&&psd.length()>1)) {
 
 
